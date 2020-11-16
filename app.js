10b1948267351599644b859a2bab6706b905dc9b
Edgar Gonçalves <32566636+Edgar-Goncalves@users.noreply.github.com>
2020-11-16 00:38:47 +0000

    Fetching and handling values from mysql db

--- app.js
+++ app.js
@@ -2,8 +2,42 @@
 const express = require('express')
 const app= express()
 const morgan= require ('morgan')
+const mysql= require("mysql")
 
 app.use(morgan('short'))
+
+//json request
+app.get("/user/:id", (req, res)=>{
+  console.log("Fetching user with id:" + req.params.id)
+
+  //db connection
+  const connection= mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "nodejstest"
+  })
+  //query
+  const userId = req.params.id
+  const queryString= "SELECT * FROM users WHERE id= ?"
+  connection.query(queryString, [userId],(err, rows, fields)=>{
+    //error handler
+    if(err){
+      console.log("Failed users query" + err)
+      res.sendStatus(500)
+      return
+    }
+    else{
+      console.log("user success")
+      //res.json(rows)
+    }
+
+    const users =rows.map((row)=>{
+      return {firstName: row.first_name, lastName: row.last_name}
+    })
+    res.json(users)
+ 
+  })
+})
 
 app.get("/", (req, res)=>{
   console.log("responding to root")
